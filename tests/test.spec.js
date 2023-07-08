b681d8eb05c0babd087122d2fe3c9c382fd904c4
godlyhedgehog <ctpax11@gmail.com>
2023-07-08 15:46:53 +0200

    Init v1
    Init v1

--- tests/test.spec.js
+++
@@ -1,8 +0,0 @@
-// test suite name
-describe('Tutorialspoint Test', function () {
-// Test case
-    it('Scenario 1', function (){
-        // test step for URL launching
-        cy.visit("https://www.google.com/");
-    });
-});
